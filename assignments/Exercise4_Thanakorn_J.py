173bda8e37273850981eb18677ff93473d941c90
nameofline <58875995+nameofline@users.noreply.github.com>
2020-05-05 01:41:56 +0700

    Add files via upload

---
+++ assignments/Exercise4_Thanakorn_J.py
@@ -0,0 +1,9 @@
+FE = 97.5
+GB = 81
+ICS = 93.5
+CP = 95
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
