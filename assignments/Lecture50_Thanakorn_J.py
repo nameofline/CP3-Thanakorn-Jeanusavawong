90258f9088aa991d3a89713f47e007d5957d73cf
nameofline <58875995+nameofline@users.noreply.github.com>
2020-05-13 01:33:05 +0700

    Add files via upload

---
+++ assignments/Lecture50_Thanakorn_J.py
@@ -0,0 +1,12 @@
+def addnumber(x,y):
+    print(x+y)
+def subtract(x,y):
+    print(x-y)
+def multiply(x,y):
+    print(x*y)
+def divide(x,y):
+    print(x/y)
+addnumber(10,15)
+subtract(20,5)
+multiply(2,7)
+divide(100,25)
